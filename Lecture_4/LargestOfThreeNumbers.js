f45a38c64eb840a4f40b77caa16f3221a29d5bc9
Riya-davra04 <riyadavra@gmail.com>
2024-06-25 20:15:46 +0530

    Largest of Three Numbers

--- Lecture_4/LargestOfThreeNumbers.js
+++ Lecture_4/LargestOfThreeNumbers.js
@@ -5,7 +5,7 @@ let num3 = 10
 if (num1 > num2 && num1 > num3) {
     console.log("Num1 is largest");
 }
-else if (num2 > num1 && num2 > num3) {
+else if (num2 > num3) {
     console.log("Num2 is largest");
 }
 else 
