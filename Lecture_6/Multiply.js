553b5519fa0789a1c18181ebc95a87fcec0a71da
Riya-davra04 <riyadavra@gmail.com>
2024-06-27 23:16:00 +0530

    Multiply by 50

--- Lecture_6/Multiply.js
+++ Lecture_6/Multiply.js
@@ -2,7 +2,7 @@ const multi = () =>
     {
         let num =document.getElementById('num').value;
 
-        let mul = num *2;
+        let mul = num *50;
 
         document.getElementById('result').innerHTML = `${num} * 2 = ${mul}`
     }
