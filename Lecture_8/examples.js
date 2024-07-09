5ebf98d16402962eba3b2cf12708a001d70a8555
Riya-davra04 <riyadavra@gmail.com>
2024-07-09 19:48:35 +0530

    use pop and toString

---
+++ Lecture_8/examples.js
@@ -0,0 +1,12 @@
+let numbers = [1, 2, 3];
+numbers.push(4);
+console.log(numbers); 
+
+
+numbers.pop();
+console.log(numbers); 
+
+console.log(numbers.length); 
+
+
+console.log(numbers.toString()); // "1,2,3"
--- Lecture_8/pop.js
+++ Lecture_8/pop.js
@@ -2,4 +2,4 @@ let array=[3,4,1,2,4];
 
 array.pop();
 
-console.log(array);
+console.log(array.toString());
