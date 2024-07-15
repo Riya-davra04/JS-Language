2c5cacf9d0b53e881ba6e24fa7069857182ef273
Riya-davra04 <riyadavra@gmail.com>
2024-07-15 10:57:19 +0530

    JS

--- Lecture_9/index.js
+++ Lecture_9/index.js
@@ -1,4 +1,21 @@
+const uimaker = () => {
+    document.getElementById('tbody').innerHTML = ''
 
+    for (let i =0 ; i<users.length ; i++)
+    {
+        let tr = document.createElement('tr')
+        let td1 = document.createElement('td')
+        td1.innerHTML = users[i].username;
+        let td2 = document.createElement('td')
+        td2.innerHTML = users[i].number;
+        let td3 = document.createElement('td')
+        td3.innerHTML = users[i].email;
+        let td4 = document.createElement('td')
+        td4.innerHTML = users[i].salary;
+        tr.append(td1,td2,td3,td4)
+        document.getElementById('tbody').append(tr)
+    }
+}
 let users = []
 
 
@@ -19,7 +36,8 @@ const handleData = (event) =>
     }
 
     users.push(user);
-    console.log(user)
+    console.log(user);
+    uimaker();
 }
 
 document.getElementById('userData').addEventListener("submit",handleData)
