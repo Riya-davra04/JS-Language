40e01ca257065f1a084fe002f84731f7ffca27e5
Riya-davra04 <riyadavra@gmail.com>
2024-07-22 22:11:06 +0530

    Exam_4

--- Exam/Exam_4/index.js
+++ Exam/Exam_4/index.js
@@ -1,13 +1,26 @@
 
 let users = []
 
-const Delete = (index) =>
-    {
-        users.splice(index,1);
-        Student();
-        
-    };
-    
+const Delete = (index) => {
+    users.splice(index, 1);
+    Student();
+    Sales()
+};
+
+const FeeAmount = (course) => {
+    if (course === "DataScience") {
+        return 75000;
+    } else if (course === "AI") {
+        return 60000;
+    } else if (course === "CyberSecurity") {
+        return 45000;
+    } else if (course === "WebDevloper") {
+        return 105000;
+    } else {
+        return 0;
+    }
+};
+
 const Student = () => {
     document.getElementById('tbody').innerHTML = ''
     for (let i = 0; i < users.length; i++) {
@@ -35,37 +48,82 @@ const Student = () => {
 const Sales = () => {
     document.getElementById('body').innerHTML = '';
 
-    let courseData = {};
+    let dataScienceCount = 0;
+    let aiCount = 0;
+    let cyberSecurityCount = 0;
+    let webDeveloperCount = 0;
+    let dataScienceTotalFees = 0;
+    let aiTotalFees = 0;
+    let cyberSecurityTotalFees = 0;
+    let webDeveloperTotalFees = 0;
 
+   
     for (let i = 0; i < users.length; i++) {
-        let user = users[i];
-        let fees = user.fees;
-        let feeAmount = "";
-        for (let j = 0; j < fees.length; j++) {
-            if (!isNaN(fees[j]) && fees[j] !== ' ') {
-                feeAmount += fees[j];
-            }
+        let course = users[i].course;
+        let fee = FeeAmount(course);
+
+        if (course === "DataScience") {
+            dataScienceCount++;
+            dataScienceTotalFees += fee;
+        } else if (course === "AI") {
+            aiCount++;
+            aiTotalFees += fee;
+        } else if (course === "CyberSecurity") {
+            cyberSecurityCount++;
+            cyberSecurityTotalFees += fee;
+        } else if (course === "WebDevloper") {
+            webDeveloperCount++;
+            webDeveloperTotalFees += fee;
         }
-        if (!courseData[user.course]) {
-            courseData[user.course] = { count: 0, totalFees: 0 };
-        }
-        courseData[user.course].count++;
-        courseData[user.course].totalFees += Number(feeAmount);
     }
 
-    for (let course in courseData) {
-        let tr = document.createElement('tr');
-        let td1 = document.createElement('td');
-        td1.innerHTML = course;
-        let td2 = document.createElement('td');
-        td2.innerHTML = courseData[course].count;
-        let td3 = document.createElement('td');
-        td3.innerHTML = courseData[course].totalFees;
+    
+    let tbody = document.getElementById('body');
 
-        tr.append(td1, td2, td3);
-        document.getElementById('body').append(tr);
-    }
-}
+   
+    let tr1 = document.createElement('tr');
+    let td1_1 = document.createElement('td');
+    td1_1.innerHTML = "DataScience";
+    let td2_1 = document.createElement('td');
+    td2_1.innerHTML = dataScienceCount;
+    let td3_1 = document.createElement('td');
+    td3_1.innerHTML = dataScienceTotalFees;
+    tr1.append(td1_1, td2_1, td3_1);
+    tbody.append(tr1);
+
+
+    let tr2 = document.createElement('tr');
+    let td1_2 = document.createElement('td');
+    td1_2.innerHTML = "AI";
+    let td2_2 = document.createElement('td');
+    td2_2.innerHTML = aiCount;
+    let td3_2 = document.createElement('td');
+    td3_2.innerHTML = aiTotalFees;
+    tr2.append(td1_2, td2_2, td3_2);
+    tbody.append(tr2);
+
+  
+    let tr3 = document.createElement('tr');
+    let td1_3 = document.createElement('td');
+    td1_3.innerHTML = "CyberSecurity";
+    let td2_3 = document.createElement('td');
+    td2_3.innerHTML = cyberSecurityCount;
+    let td3_3 = document.createElement('td');
+    td3_3.innerHTML = cyberSecurityTotalFees;
+    tr3.append(td1_3, td2_3, td3_3);
+    tbody.append(tr3);
+
+ 
+    let tr4 = document.createElement('tr');
+    let td1_4 = document.createElement('td');
+    td1_4.innerHTML = "WebDevloper";
+    let td2_4 = document.createElement('td');
+    td2_4.innerHTML = webDeveloperCount;
+    let td3_4 = document.createElement('td');
+    td3_4.innerHTML = webDeveloperTotalFees;
+    tr4.append(td1_4, td2_4, td3_4);
+    tbody.append(tr4);
+};
 
 const HandleData = (e) => {
     e.preventDefault();
@@ -109,6 +167,7 @@ const HandleData = (e) => {
     users.push(user);
     console.log(user)
     Student();
+    Sales();
 }
 
 document.getElementById('userData').addEventListener("submit", HandleData)
