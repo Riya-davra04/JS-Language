89aa60888ff1a3981bec51a04edafbd1b9964a53
Riya-davra04 <riyadavra@gmail.com>
2024-07-29 21:47:35 +0530

    Exam_5

--- Exam/Exam_5/ascending.js
+++ Exam/Exam_5/ascending.js
@@ -1,8 +1,9 @@
 let arr =[3,2,1,4,5]
 
-let asc = arr.slice().sort((a, b) => a - b);
-
-let sort = arr.toString() === asc.toString();
-
+let asc = true;
+for (let i = 0; i < arr.length - 1; i++) {
+  if (arr[i] > arr[i + 1]) {
+    asc = false;
+  }
+}
 console.log(asc);
-console.log(sort);
