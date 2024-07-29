bd6a3cf289242ba12975c3351c8f89c15bf2c2fc
Riya-davra04 <riyadavra@gmail.com>
2024-07-29 22:01:40 +0530

    Exam_4

--- Exam/Exam_5/rotate.js
+++ Exam/Exam_5/rotate.js
@@ -1,10 +1,11 @@
 let arr = [1, 2, 3, 4, 5];
 let positions = 2;
-let rotated = [];
+let temporaryArray = [];
 
-for (let i = 0; i < arr.length; i++) {
-  let temporaryArray ;
-  
-  rotated[temporaryArray] = arr[i];
-}
-console.log(rotated);
+let effectiveNumber = positions % arr.length;
+
+let end = arr.slice(arr.length - effectiveNumber);
+
+let start = arr.slice(arr.length - effectiveNumber);
+
+console.log(temporaryArray);
