4ab7b9ff63e88ac8388d5bb933873c08d8ffa2e3
Riya-davra04 <riyadavra@gmail.com>
2024-08-06 10:47:58 +0530

    Exam_6

--- Exam/Exam_6/book.js
+++ Exam/Exam_6/book.js
@@ -1,110 +1,96 @@
-class Book
-{
+class Book {
     title;
     #author;
     #price;
     #rentalPrice;
     #copiesAvailable;
-    constructor(title,author,price,rentalPrice,copiesAvailable)
-    {
-        this.title=title;
-        this.#author=author;
-        this.#price=price;
-        this.#rentalPrice=rentalPrice;
-        this.#copiesAvailable=copiesAvailable;
+
+    constructor(title, author, price, rentalPrice, copiesAvailable) {
+        this.title = title;
+        this.#author = author;
+        this.#price = price;
+        this.#rentalPrice = rentalPrice;
+        this.#copiesAvailable = copiesAvailable;
     }
 
-    getauthor()
-    {
+    getauthor() {
         return this.#author;
     }
 
-    setauthor(author)
-    {
-        this.#author=author;
+    setauthor(author) {
+        this.#author = author;
     }
 
-    getprice()
-    {
+    getprice() {
         return this.#price;
     }
 
-    setprice(price)
-    {
-        this.#price=price;
+    setprice(price) {
+        this.#price = price;
     }
 
-    getrentalPrice()
-    {
+    getrentalPrice() {
         return this.#rentalPrice;
     }
 
-    setrentalPrice(rentalPrice)
-    {
-        this.#rentalPrice=rentalPrice;
+    setrentalPrice(rentalPrice) {
+        this.#rentalPrice = rentalPrice;
     }
 
-    getcopiesAvailable()
-    {
+    getcopiesAvailable() {
         return this.#copiesAvailable;
     }
 
-    setcopiesAvailable(copiesAvailable)
-    {
-        this.#copiesAvailable=copiesAvailable;
+    setcopiesAvailable(copiesAvailable) {
+        this.#copiesAvailable = copiesAvailable;
     }
 
-    buyBook(copies,bookprice)
-    {
-        if(this.#copiesAvailable>0)
-        {
-            this.#copiesAvailable-=copies;
-            this.#price+=bookprice;
-            console.log(`${this.title} Book purchased and book price is ${this.#price}`);
-        }
-        else
-        {
-            console.log("Sorry! copies are not available");
+    buyBook(copies) {
+        if (copies > 0 && this.#copiesAvailable >= copies) {
+            this.#copiesAvailable -= copies;
+            let totalPrice = copies * this.#price;
+            console.log(`${this.title} Book purchased. Total price is ${totalPrice}`);
+        } else {
+            console.log("Sorry! Not enough copies are available");
         }
     }
-    rentBook(copies,retprice)
-    {
-        if(this.#copiesAvailable>0)
-        {
-            this.#copiesAvailable-=copies;
-            this.#rentalPrice+=retprice;
-            console.log(`${this.title} Book rented and book rentprice is ${this.#rentalPrice}`);
-            }
-            else
-            {
-                console.log("Sorry! copies are not available");
-            }
+
+    rentBook(copies) {
+        if (copies > 0 && this.#copiesAvailable >= copies) {
+            this.#copiesAvailable -= copies;
+            let totalRentalPrice = copies * this.#rentalPrice;
+            console.log(`${this.title} Book rented. Total rental price is ${totalRentalPrice}`);
+        } else {
+            console.log("Sorry! Not enough copies are available");
+        }
     }
 
-    returnBook(copies)
-    {
-        this.#copiesAvailable+=copies;
-        console.log(`${this.title} Book is return`);
+    returnBook(copies) {
+        if (copies > 0) {
+            this.#copiesAvailable += copies;
+            console.log(`${this.title} Book is returned`);
+        } else {
+            console.log("Invalid number of copies to return");
+        }
     }
 
-    checkAvailability()
-    {
-        console.log(`Total ${this.#copiesAvailable} Copies are available `);
+    checkAvailability() {
+        console.log(`Total ${this.#copiesAvailable} copies are available`);
     }
 }
 
-let book = new Book("The Story of My Experiments With Truth"," Mahatma Gandhi",350,100,14);
+let book = new Book("The Story of My Experiments With Truth", "Mahatma Gandhi", 350, 100, 14);
 console.log(book);
-console.log("Author name  : ",book.getauthor());
-console.log("Price :",book.getprice());
-console.log("RentalPrice : ",book.getrentalPrice());
-console.log("CopiesAvailable : ",book.getcopiesAvailable());
+console.log("Author name  : ", book.getauthor());
+console.log("Price :", book.getprice());
+console.log("RentalPrice : ", book.getrentalPrice());
+console.log("CopiesAvailable : ", book.getcopiesAvailable());
 
-book.buyBook(2,350);
+book.buyBook(4);
 book.checkAvailability();
 
-book.rentBook(2,100);
+book.rentBook(3);
 book.checkAvailability();
 
-book.returnBook(3);
-book.checkAvailability();``
+book.returnBook(1);
+book.checkAvailability();
