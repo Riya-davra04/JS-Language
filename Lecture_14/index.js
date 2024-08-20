299ac76958efc6bc6c30808adc57c610fafbd046
Riya-davra04 <riyadavra@gmail.com>
2024-08-20 19:48:10 +0530

    Counter

---
+++ Lecture_14/index.js
@@ -0,0 +1,24 @@
+let counters = {
+    insta: 0,
+    snap: 0,
+    fb: 0,
+    twitter: 0
+};
+
+function incrementCounter(platform) {
+    counters[platform]++;
+    document.getElementById(platform + '-counter').innerText = counters[platform];
+}
+
+function startCounter(platform, delay) {
+    setTimeout(function() {
+        setInterval(function() {
+            incrementCounter(platform);
+        }, 100); 
+    }, delay);
+}
+
+startCounter('insta', 0);        
+startCounter('snap', 1000);     
+startCounter('fb', 2000);        
+startCounter('twitter', 3000);  
