aa2258e72bb9fbcf74c7a4bc03992de4f205c296
Riya-davra04 <riyadavra@gmail.com>
2024-08-20 22:12:16 +0530

    Dice

---
+++ Lecture_15/index.js
@@ -0,0 +1,18 @@
+
+ rollDice = () => {
+    let dice = document.getElementById('dice');
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    
+    let current = 1;
+    let intervalId = setInterval(() => {
+        dice.textContent = current;
+        currentFace = current % 6 + 1; 
+    }, 30);
+    
+    setTimeout(() => {
+        clearInterval(intervalId);
+        dice.textContent = randomNumber;
+    }, 1000); 
+}
+
+document.getElementById('rollButton').addEventListener('click', rollDice);
